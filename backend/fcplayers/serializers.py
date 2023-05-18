3e8a38261abef5cada7872e9dd0ad4cf696dde90
Sydney McGee <mcgee.sydney@gmail.com>
2023-05-17 22:41:54 -0500

    Update project urls + created first view function + created players urls file

---
+++ backend/fcplayers/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import Fcplayers
+
+class FcplayersSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Fcplayers
+        fields = ['id', 'firstname', 'lastname', 'age', 'parent_id']
